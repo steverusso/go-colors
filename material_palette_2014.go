568ad2cb220bb1ade3480e1d4cf3c5b8f74c49f5
Steve Russo <steverusso@pm.me>
2022-12-12 19:39:33 -0500

    add material palette colors

---
+++ material_palette_2014.go
@@ -0,0 +1,349 @@
+package colors
+
+import "image/color"
+
+// Red.
+var (
+	Red50  = color.NRGBA{255, 235, 238, 255}
+	Red100 = color.NRGBA{255, 205, 210, 255}
+	Red200 = color.NRGBA{239, 154, 154, 255}
+	Red300 = color.NRGBA{229, 115, 115, 255}
+	Red400 = color.NRGBA{239, 83, 80, 255}
+	Red500 = color.NRGBA{244, 67, 54, 255}
+	Red600 = color.NRGBA{229, 57, 53, 255}
+	Red700 = color.NRGBA{211, 47, 47, 255}
+	Red800 = color.NRGBA{198, 40, 40, 255}
+	Red900 = color.NRGBA{183, 28, 28, 255}
+
+	RedA100 = color.NRGBA{255, 138, 128, 255}
+	RedA200 = color.NRGBA{255, 82, 82, 255}
+	RedA400 = color.NRGBA{255, 23, 68, 255}
+	RedA700 = color.NRGBA{213, 0, 0, 255}
+)
+
+// Pink.
+var (
+	Pink50  = color.NRGBA{252, 228, 236, 255}
+	Pink100 = color.NRGBA{248, 187, 208, 255}
+	Pink200 = color.NRGBA{244, 143, 177, 255}
+	Pink300 = color.NRGBA{240, 98, 146, 255}
+	Pink400 = color.NRGBA{236, 64, 122, 255}
+	Pink500 = color.NRGBA{233, 30, 99, 255}
+	Pink600 = color.NRGBA{216, 27, 96, 255}
+	Pink700 = color.NRGBA{194, 24, 91, 255}
+	Pink800 = color.NRGBA{173, 20, 87, 255}
+	Pink900 = color.NRGBA{136, 14, 79, 255}
+
+	PinkA100 = color.NRGBA{255, 128, 171, 255}
+	PinkA200 = color.NRGBA{255, 64, 129, 255}
+	PinkA400 = color.NRGBA{245, 0, 87, 255}
+	PinkA700 = color.NRGBA{197, 17, 98, 255}
+)
+
+// Purple.
+var (
+	Purple50  = color.NRGBA{243, 229, 245, 255}
+	Purple100 = color.NRGBA{225, 190, 231, 255}
+	Purple200 = color.NRGBA{206, 147, 216, 255}
+	Purple300 = color.NRGBA{186, 104, 200, 255}
+	Purple400 = color.NRGBA{171, 71, 188, 255}
+	Purple500 = color.NRGBA{156, 39, 176, 255}
+	Purple600 = color.NRGBA{142, 36, 170, 255}
+	Purple700 = color.NRGBA{123, 31, 162, 255}
+	Purple800 = color.NRGBA{106, 27, 154, 255}
+	Purple900 = color.NRGBA{74, 20, 140, 255}
+
+	PurpleA100 = color.NRGBA{74, 20, 140, 255}
+	PurpleA200 = color.NRGBA{224, 64, 251, 255}
+	PurpleA400 = color.NRGBA{213, 0, 249, 255}
+	PurpleA700 = color.NRGBA{170, 0, 255, 255}
+)
+
+// Depp purple.
+var (
+	DeepPurple50  = color.NRGBA{237, 231, 246, 255}
+	DeepPurple100 = color.NRGBA{209, 196, 233, 255}
+	DeepPurple200 = color.NRGBA{179, 157, 219, 255}
+	DeepPurple300 = color.NRGBA{149, 117, 205, 255}
+	DeepPurple400 = color.NRGBA{126, 87, 194, 255}
+	DeepPurple500 = color.NRGBA{103, 58, 183, 255}
+	DeepPurple600 = color.NRGBA{94, 53, 177, 255}
+	DeepPurple700 = color.NRGBA{81, 45, 168, 255}
+	DeepPurple800 = color.NRGBA{69, 39, 160, 255}
+	DeepPurple900 = color.NRGBA{49, 27, 146, 255}
+
+	DeepPurpleA100 = color.NRGBA{179, 136, 255, 255}
+	DeepPurpleA200 = color.NRGBA{124, 77, 255, 255}
+	DeepPurpleA400 = color.NRGBA{101, 31, 255, 255}
+	DeepPurpleA700 = color.NRGBA{98, 0, 234, 255}
+)
+
+// Indigo.
+var (
+	Indigo50  = color.NRGBA{232, 234, 246, 255}
+	Indigo100 = color.NRGBA{197, 202, 233, 255}
+	Indigo200 = color.NRGBA{159, 168, 218, 255}
+	Indigo300 = color.NRGBA{121, 134, 203, 255}
+	Indigo400 = color.NRGBA{92, 107, 192, 255}
+	Indigo500 = color.NRGBA{63, 81, 181, 255}
+	Indigo600 = color.NRGBA{57, 73, 171, 255}
+	Indigo700 = color.NRGBA{48, 63, 159, 255}
+	Indigo800 = color.NRGBA{40, 53, 147, 255}
+	Indigo900 = color.NRGBA{26, 35, 126, 255}
+
+	IndigoA100 = color.NRGBA{140, 158, 255, 255}
+	IndigoA200 = color.NRGBA{83, 109, 254, 255}
+	IndigoA400 = color.NRGBA{61, 90, 254, 255}
+	IndigoA700 = color.NRGBA{48, 79, 254, 255}
+)
+
+// Blue.
+var (
+	Blue50  = color.NRGBA{227, 242, 253, 255}
+	Blue100 = color.NRGBA{187, 222, 251, 255}
+	Blue200 = color.NRGBA{144, 202, 249, 255}
+	Blue300 = color.NRGBA{100, 181, 246, 255}
+	Blue400 = color.NRGBA{66, 165, 245, 255}
+	Blue500 = color.NRGBA{33, 150, 243, 255}
+	Blue600 = color.NRGBA{30, 136, 229, 255}
+	Blue700 = color.NRGBA{25, 118, 210, 255}
+	Blue800 = color.NRGBA{21, 101, 192, 255}
+	Blue900 = color.NRGBA{13, 71, 161, 255}
+
+	BlueA100 = color.NRGBA{130, 177, 255, 255}
+	BlueA200 = color.NRGBA{68, 138, 255, 255}
+	BlueA400 = color.NRGBA{41, 121, 255, 255}
+	BlueA700 = color.NRGBA{41, 98, 255, 255}
+)
+
+// Light blue.
+var (
+	LightBlue50  = color.NRGBA{225, 245, 254, 255}
+	LightBlue100 = color.NRGBA{179, 229, 252, 255}
+	LightBlue200 = color.NRGBA{129, 212, 250, 255}
+	LightBlue300 = color.NRGBA{79, 195, 247, 255}
+	LightBlue400 = color.NRGBA{41, 182, 246, 255}
+	LightBlue500 = color.NRGBA{3, 169, 244, 255}
+	LightBlue600 = color.NRGBA{3, 155, 229, 255}
+	LightBlue700 = color.NRGBA{2, 136, 209, 255}
+	LightBlue800 = color.NRGBA{2, 119, 189, 255}
+	LightBlue900 = color.NRGBA{1, 87, 155, 255}
+
+	LightBlueA100 = color.NRGBA{128, 216, 255, 255}
+	LightBlueA200 = color.NRGBA{64, 196, 255, 255}
+	LightBlueA400 = color.NRGBA{0, 176, 255, 255}
+	LightBlueA700 = color.NRGBA{0, 145, 234, 255}
+)
+
+// Cyan.
+var (
+	Cyan50  = color.NRGBA{224, 247, 250, 255}
+	Cyan100 = color.NRGBA{178, 235, 242, 255}
+	Cyan200 = color.NRGBA{128, 222, 234, 255}
+	Cyan300 = color.NRGBA{77, 208, 225, 255}
+	Cyan400 = color.NRGBA{38, 198, 218, 255}
+	Cyan500 = color.NRGBA{0, 188, 212, 255}
+	Cyan600 = color.NRGBA{0, 172, 193, 255}
+	Cyan700 = color.NRGBA{0, 151, 167, 255}
+	Cyan800 = color.NRGBA{0, 131, 143, 255}
+	Cyan900 = color.NRGBA{0, 96, 100, 255}
+
+	CyanA100 = color.NRGBA{132, 255, 255, 255}
+	CyanA200 = color.NRGBA{24, 255, 255, 255}
+	CyanA400 = color.NRGBA{0, 229, 255, 255}
+	CyanA700 = color.NRGBA{0, 184, 212, 255}
+)
+
+// Teal.
+var (
+	Teal50  = color.NRGBA{224, 242, 241, 255}
+	Teal100 = color.NRGBA{178, 223, 219, 255}
+	Teal200 = color.NRGBA{128, 203, 196, 255}
+	Teal300 = color.NRGBA{77, 182, 172, 255}
+	Teal400 = color.NRGBA{38, 166, 154, 255}
+	Teal500 = color.NRGBA{0, 150, 136, 255}
+	Teal600 = color.NRGBA{0, 137, 123, 255}
+	Teal700 = color.NRGBA{0, 121, 107, 255}
+	Teal800 = color.NRGBA{0, 105, 92, 255}
+	Teal900 = color.NRGBA{0, 77, 64, 255}
+
+	TealA100 = color.NRGBA{167, 255, 235, 255}
+	TealA200 = color.NRGBA{100, 255, 218, 255}
+	TealA400 = color.NRGBA{29, 233, 182, 255}
+	TealA700 = color.NRGBA{0, 191, 165, 255}
+)
+
+// Green.
+var (
+	Green50  = color.NRGBA{232, 245, 233, 255}
+	Green100 = color.NRGBA{200, 230, 201, 255}
+	Green200 = color.NRGBA{165, 214, 167, 255}
+	Green300 = color.NRGBA{129, 199, 132, 255}
+	Green400 = color.NRGBA{102, 187, 106, 255}
+	Green500 = color.NRGBA{76, 175, 80, 255}
+	Green600 = color.NRGBA{67, 160, 71, 255}
+	Green700 = color.NRGBA{56, 142, 60, 255}
+	Green800 = color.NRGBA{46, 125, 50, 255}
+	Green900 = color.NRGBA{27, 94, 32, 255}
+
+	GreenA100 = color.NRGBA{185, 246, 202, 255}
+	GreenA200 = color.NRGBA{105, 240, 174, 255}
+	GreenA400 = color.NRGBA{0, 230, 118, 255}
+	GreenA700 = color.NRGBA{0, 200, 83, 255}
+)
+
+// Light green.
+var (
+	LightGreen50  = color.NRGBA{241, 248, 233, 255}
+	LightGreen100 = color.NRGBA{220, 237, 200, 255}
+	LightGreen200 = color.NRGBA{197, 225, 165, 255}
+	LightGreen300 = color.NRGBA{174, 213, 129, 255}
+	LightGreen400 = color.NRGBA{156, 204, 101, 255}
+	LightGreen500 = color.NRGBA{139, 195, 74, 255}
+	LightGreen600 = color.NRGBA{124, 179, 66, 255}
+	LightGreen700 = color.NRGBA{104, 159, 56, 255}
+	LightGreen800 = color.NRGBA{85, 139, 47, 255}
+	LightGreen900 = color.NRGBA{51, 105, 30, 255}
+
+	LightGreenA100 = color.NRGBA{204, 255, 144, 255}
+	LightGreenA200 = color.NRGBA{178, 255, 89, 255}
+	LightGreenA400 = color.NRGBA{118, 255, 3, 255}
+	LightGreenA700 = color.NRGBA{100, 221, 23, 255}
+)
+
+// Lime.
+var (
+	Lime50  = color.NRGBA{249, 251, 231, 255}
+	Lime100 = color.NRGBA{240, 244, 195, 255}
+	Lime200 = color.NRGBA{230, 238, 156, 255}
+	Lime300 = color.NRGBA{220, 231, 117, 255}
+	Lime400 = color.NRGBA{212, 225, 87, 255}
+	Lime500 = color.NRGBA{205, 220, 57, 255}
+	Lime600 = color.NRGBA{192, 202, 51, 255}
+	Lime700 = color.NRGBA{175, 180, 43, 255}
+	Lime800 = color.NRGBA{158, 157, 36, 255}
+	Lime900 = color.NRGBA{130, 119, 23, 255}
+
+	LimeA100 = color.NRGBA{244, 255, 129, 255}
+	LimeA200 = color.NRGBA{238, 255, 65, 255}
+	LimeA400 = color.NRGBA{198, 255, 0, 255}
+	LimeA700 = color.NRGBA{174, 234, 0, 255}
+)
+
+// Yellow.
+var (
+	Yellow50  = color.NRGBA{255, 253, 231, 255}
+	Yellow100 = color.NRGBA{255, 249, 196, 255}
+	Yellow200 = color.NRGBA{255, 245, 157, 255}
+	Yellow300 = color.NRGBA{255, 241, 118, 255}
+	Yellow400 = color.NRGBA{255, 238, 88, 255}
+	Yellow500 = color.NRGBA{255, 235, 59, 255}
+	Yellow600 = color.NRGBA{253, 216, 53, 255}
+	Yellow700 = color.NRGBA{251, 192, 45, 255}
+	Yellow800 = color.NRGBA{249, 168, 37, 255}
+	Yellow900 = color.NRGBA{245, 127, 23, 255}
+
+	YellowA100 = color.NRGBA{255, 255, 141, 255}
+	YellowA200 = color.NRGBA{255, 255, 0, 255}
+	YellowA400 = color.NRGBA{255, 234, 0, 255}
+	YellowA700 = color.NRGBA{255, 214, 0, 255}
+)
+
+// Amber.
+var (
+	Amber50  = color.NRGBA{255, 248, 225, 255}
+	Amber100 = color.NRGBA{255, 236, 179, 255}
+	Amber200 = color.NRGBA{255, 224, 130, 255}
+	Amber300 = color.NRGBA{255, 213, 79, 255}
+	Amber400 = color.NRGBA{255, 202, 40, 255}
+	Amber500 = color.NRGBA{255, 193, 7, 255}
+	Amber600 = color.NRGBA{255, 179, 0, 255}
+	Amber700 = color.NRGBA{255, 160, 0, 255}
+	Amber800 = color.NRGBA{255, 143, 0, 255}
+	Amber900 = color.NRGBA{255, 111, 0, 255}
+
+	AmberA100 = color.NRGBA{255, 229, 127, 255}
+	AmberA200 = color.NRGBA{255, 215, 64, 255}
+	AmberA400 = color.NRGBA{255, 196, 0, 255}
+	AmberA700 = color.NRGBA{255, 171, 0, 255}
+)
+
+// Orange.
+var (
+	Orange50  = color.NRGBA{255, 243, 224, 255}
+	Orange100 = color.NRGBA{255, 224, 178, 255}
+	Orange200 = color.NRGBA{255, 204, 128, 255}
+	Orange300 = color.NRGBA{255, 183, 77, 255}
+	Orange400 = color.NRGBA{255, 167, 38, 255}
+	Orange500 = color.NRGBA{255, 152, 0, 255}
+	Orange600 = color.NRGBA{251, 140, 0, 255}
+	Orange700 = color.NRGBA{245, 124, 0, 255}
+	Orange800 = color.NRGBA{239, 108, 0, 255}
+	Orange900 = color.NRGBA{230, 81, 0, 255}
+
+	OrangeA100 = color.NRGBA{255, 209, 128, 255}
+	OrangeA200 = color.NRGBA{255, 171, 64, 255}
+	OrangeA400 = color.NRGBA{255, 145, 0, 255}
+	OrangeA700 = color.NRGBA{255, 109, 0, 255}
+)
+
+// Deep orange.
+var (
+	DeepOrange50  = color.NRGBA{251, 233, 231, 255}
+	DeepOrange100 = color.NRGBA{255, 204, 188, 255}
+	DeepOrange200 = color.NRGBA{255, 171, 145, 255}
+	DeepOrange300 = color.NRGBA{255, 138, 101, 255}
+	DeepOrange400 = color.NRGBA{255, 112, 67, 255}
+	DeepOrange500 = color.NRGBA{255, 87, 34, 255}
+	DeepOrange600 = color.NRGBA{244, 81, 30, 255}
+	DeepOrange700 = color.NRGBA{230, 74, 25, 255}
+	DeepOrange800 = color.NRGBA{216, 67, 21, 255}
+	DeepOrange900 = color.NRGBA{191, 54, 12, 255}
+
+	DeepOrangeA100 = color.NRGBA{255, 158, 128, 255}
+	DeepOrangeA200 = color.NRGBA{255, 110, 64, 255}
+	DeepOrangeA400 = color.NRGBA{255, 61, 0, 255}
+	DeepOrangeA700 = color.NRGBA{221, 44, 0, 255}
+)
+
+// Brown.
+var (
+	Brown50  = color.NRGBA{239, 235, 233, 255}
+	Brown100 = color.NRGBA{215, 204, 200, 255}
+	Brown200 = color.NRGBA{188, 170, 164, 255}
+	Brown300 = color.NRGBA{161, 136, 127, 255}
+	Brown400 = color.NRGBA{141, 110, 99, 255}
+	Brown500 = color.NRGBA{121, 85, 72, 255}
+	Brown600 = color.NRGBA{109, 76, 65, 255}
+	Brown700 = color.NRGBA{93, 64, 55, 255}
+	Brown800 = color.NRGBA{78, 52, 46, 255}
+	Brown900 = color.NRGBA{62, 39, 35, 255}
+)
+
+// Grey.
+var (
+	Grey50  = color.NRGBA{250, 250, 250, 255}
+	Grey100 = color.NRGBA{245, 245, 245, 255}
+	Grey200 = color.NRGBA{238, 238, 238, 255}
+	Grey300 = color.NRGBA{224, 224, 224, 255}
+	Grey400 = color.NRGBA{189, 189, 189, 255}
+	Grey500 = color.NRGBA{158, 158, 158, 255}
+	Grey600 = color.NRGBA{117, 117, 117, 255}
+	Grey700 = color.NRGBA{97, 97, 97, 255}
+	Grey800 = color.NRGBA{66, 66, 66, 255}
+	Grey900 = color.NRGBA{33, 33, 33, 255}
+)
+
+// Blue grey.
+var (
+	BlueGrey50  = color.NRGBA{236, 239, 241, 255}
+	BlueGrey100 = color.NRGBA{207, 216, 220, 255}
+	BlueGrey200 = color.NRGBA{176, 190, 197, 255}
+	BlueGrey300 = color.NRGBA{144, 164, 174, 255}
+	BlueGrey400 = color.NRGBA{120, 144, 156, 255}
+	BlueGrey500 = color.NRGBA{96, 125, 139, 255}
+	BlueGrey600 = color.NRGBA{84, 110, 122, 255}
+	BlueGrey700 = color.NRGBA{69, 90, 100, 255}
+	BlueGrey800 = color.NRGBA{55, 71, 79, 255}
+	BlueGrey900 = color.NRGBA{38, 50, 56, 255}
+)
